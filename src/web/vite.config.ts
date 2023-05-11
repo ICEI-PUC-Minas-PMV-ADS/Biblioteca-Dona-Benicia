3cca73efcb0eb48119ed66bcabeedd0518f320a7
Daiane Rocha <dr465796@gmail.com>
2023-05-11 23:32:37 +0200

    fix vite https

--- src/web/vite.config.ts
+++ src/web/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       '/api': {
         target: `${FAST_API}`,
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
