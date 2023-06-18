c7b9f09d9c8b4a032425d2f277a0af63c6a2b5cf
Daiane Rocha <dr465796@gmail.com>
2023-06-19 01:23:51 +0200

    jwt ok

--- src/backend/app/view/login.py
+++ src/backend/app/view/login.py
@@ -78,6 +78,6 @@ def login(form_data: OAuth2PasswordRequestForm = Depends()):
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     print(user)
     access_token = create_access_token(
-        data={"sub": user["_id"]}, expires_delta=access_token_expires
+        data={"sub": str(user["_id"])}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
