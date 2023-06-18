e0a90328f018e783b2f51a3c4f5f3b9116058524
Daiane Rocha <dr465796@gmail.com>
2023-06-19 01:12:09 +0200

    include id on jwt

--- src/backend/app/view/login.py
+++ src/backend/app/view/login.py
@@ -17,11 +17,14 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  # Update the token URL if needed
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="token")  # Update the token URL if needed
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
 
 def authenticate_user(username: str, password: str):
     user = controller_usuarios.get_by_username(username)
@@ -30,6 +33,7 @@ def authenticate_user(username: str, password: str):
     if not verify_password(password, user["senha"]):
         return False
     return user
+
 
 def create_access_token(data: dict, expires_delta: timedelta):
     to_encode = data.copy()
@@ -40,6 +44,7 @@ def create_access_token(data: dict, expires_delta: timedelta):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
@@ -61,15 +66,18 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     return user
 
 # Define the login route
+
+
 @router.post("/token")
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
-        raise HTTPException(status_code=401, detail="Incorrect username or password")
-    
+        raise HTTPException(
+            status_code=401, detail="Incorrect username or password")
+
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     print(user)
     access_token = create_access_token(
-        data={"sub": user["username"]}, expires_delta=access_token_expires
+        data={"sub": user["username"], 'id': user["_id"]}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
